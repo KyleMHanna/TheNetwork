85ccb941a1a8c20b43e5f9316ac121645a270696
Kyle Hanna <Hanna.Kyle@live.com>
2021-09-24 22:52:18 -0600

    account page updates working

--- src/pages/AccountPage.vue
+++ src/pages/AccountPage.vue
@@ -4,23 +4,52 @@
     <img class="rounded" :src="account.picture" alt="//placehold.it/300x300" />
     <p>{{ account.email }}</p>
 
-    <p>Bio:{{ account.bio }}</p>
-    <p>Github:{{ account.github }}</p>
-    <p>Linkedin:{{ account.linkedin }}</p>
-    <p>Resume:{{ account.resume }}</p>
-    <p>Graduated: {{ account.graduated }}</p>
+    <p>{{ account.bio }}</p>
+    <p>{{ account.github }}</p>
+    <p>{{ account.linkedin }}</p>
+    <p>{{ account.resume }}</p>
+    <p>{{ account.graduated }}</p>
     <p>{{ account.class }}</p>
+  </div>
+  <div class="row">
+    <form @submit.prevent="updateAccount()">
+      <h3> cover image</h3>
+      <input type="text" v-model="account.coverImg"><br />
+      <h3> name</h3>
+      <input type="text" v-model="account.name" placeholder="Name"><br />
+      <h3> profile Pic</h3>
+      <input type="text" v-model="account.picture" placholder="Picture"><br />
+      <h3>  your Github</h3>
+      <input type="text" v-model="account.github" placeholder="Github"><br />
+      <h3>  your Linkedin</h3>
+      <input type="text" v-model="account.linkedin" placeholder="Linkedin"><br />
+      <h3>  graduated?</h3>
+      <input type="checkbox" v-model="account.graduated" placeholder="Graduate"><br />
+      <button class="btn btn-success ms-1">
+        Submit
+      </button>
+    </form>
   </div>
 </template>
 
 <script>
 import { computed } from 'vue'
 import { AppState } from '../AppState'
+import Pop from '../utils/Pop'
+import { accountService } from '../services/AccountService'
 export default {
   name: 'Account',
   setup() {
+    const account = computed(() => AppState.account)
     return {
-      account: computed(() => AppState.account)
+      account,
+      async updateAccount() {
+        try {
+          await accountService.editAccount(account.value)
+        } catch (error) {
+          Pop.toast(error, 'error')
+        }
+      }
     }
   }
 }
--- src/services/AccountService.js
+++ src/services/AccountService.js
@@ -11,6 +11,12 @@ class AccountService {
       logger.error('HAVE YOU STARTED YOUR SERVER YET???', err)
     }
   }
+
+  async editAccount(accountData) {
+    const res = await api.put('/account', accountData)
+    logger.log('edit Harrison would be proud', res)
+    AppState.account = res.data
+  }
 }
 
 export const accountService = new AccountService()
